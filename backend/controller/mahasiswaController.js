bac9ae42f99fe99134af83ef21f1ae7493fbf628
maliefrr <aliefm144@gmail.com>
2022-12-25 18:41:51 +0800

    add getAllMahasiswa function,route and test

--- backend/controller/mahasiswaController.js
+++ backend/controller/mahasiswaController.js
@@ -4,26 +4,46 @@ const bcrypt = require("bcryptjs");
 const uploader = require("imgbb-uploader")
 const userModel = require("../models/userModel");
 
-const addMahasiswa = asyncHandler( async (req,res) => {
-    const {name,prodi,nim,alamat,gol_darah,jenis_kelamin} = req.body;
 
-    const role = req.user.id;
-    const salt = await bcrypt.genSalt(10);
-    const authorizeUser = await userModel.findById(role)
-    const filePath = req.file.path;
-    
-    if(authorizeUser.role !== "admin") {
+const getAllMahasiswa = asyncHandler(async (req,res) => {
+    const authorizeUser = req.user.id;
+    const user = await userModel.findById(authorizeUser)
+    if(authorizeUser && user.role !== "admin") {
         res.status(401).json({
             statusCode: 401,
             message: "User not authorized"
         })
+    } else {
+        const data = await mahasiswaModel.find()
+        res.status(200).json({
+            statusCode: 200,
+            data : data.map(user => {
+                return {
+                    name : user.name,
+                    nim : user.nim,
+                    prodi : user.prodi,
+                    alamat : user.alamat,
+                    gol_darah : user.gol_darah,
+                    pas_foto : user.pas_foto,
+                    jenis_kelamin : user.jenis_kelamin
+                }
+            })
+        })
+    }
+})
+
+const addMahasiswa = asyncHandler( async (req,res) => {
+    const {name,prodi,nim,alamat,gol_darah,jenis_kelamin} = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const filePath = req.file.path;
+    
     if(!name || !prodi || !nim || !alamat) {
         res.status(400).json({
             statusCode: 400,
             message: "The field cannot be blank"
         })
     }
-    } else {
+    else {
         const imageUpload = await uploader(process.env.IMG_API, filePath)
         const data = await mahasiswaModel.create({
             name,prodi,alamat,pas_foto : imageUpload.image.url, nim,
@@ -49,5 +69,5 @@ const addMahasiswa = asyncHandler( async (req,res) => {
 
 
 module.exports = {
-    addMahasiswa
+    addMahasiswa, getAllMahasiswa
 }
--- backend/routes/mahasiswaRoute.js
+++ backend/routes/mahasiswaRoute.js
@@ -1,7 +1,8 @@
-const { addMahasiswa } = require("../controller/mahasiswaController")
+const { addMahasiswa, getAllMahasiswa } = require("../controller/mahasiswaController")
 const protect = require("../middleware/authHandler")
 const router = require("express").Router()
 
-router.post("/add", protect ,addMahasiswa)
+router.get("/all",protect,getAllMahasiswa)
+router.post("/add",addMahasiswa)
 
 module.exports = router
