b9844215139ff05c92b1bc844cc2ef0557ead5f3
maliefrr <aliefm144@gmail.com>
2023-02-11 15:04:13 +0800

    fix profile pages function but the submit just return on the console

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -8,7 +8,13 @@ import Button from '../components/Button'
 import InputForm from '../components/InputForm'
 
 const Profile = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({
+    name : "",
+    prodi : "",
+    nim : "",
+    gol_darah : "",
+    jenis_kelamin : ""
+  });
   const {user} = useSelector((state) => state.auth)
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -18,7 +24,13 @@ const Profile = () => {
       setIsLoading(true);
       try {
           const response = await axios.get(`http://localhost:5000/api/mahasiswa/profile/${user.data.username}`);
-          setData(response.data.mahasiswa);
+          setData({
+            name: response.data.mahasiswa.name,
+            prodi: response.data.mahasiswa.prodi,
+            nim: response.data.mahasiswa.nim,
+            gol_darah: response.data.mahasiswa.gol_darah,
+            jenis_kelamin: response.data.mahasiswa.jenis_kelamin,
+          });
       } catch (err) {
           setError(err);
       } finally {
@@ -45,6 +57,10 @@ const Profile = () => {
   const handleChange = event => {
     setData({ ...data, [event.target.name]: event.target.value });
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
   return (
     <div className="flex h-screen">
         <SideBar/>
@@ -55,15 +71,50 @@ const Profile = () => {
               <>
                 <img src={data.pas_foto} alt={`Profile ${name}`} className="w-52 h-52 rounded-md object-cover mx-auto"/>
                 <hr className=' border-2 border-black my-3'/>
+                <h1 className="text-center font-bold text-2xl">Informasi Akun</h1>
                 <div className="flex justify-end">
                   <Button className="" text="Edit" onClick={() => setEdit(!edit)}/>
                 </div>
-                <InputForm id='nama' type='text' name='name' placeholder="Nama" label="Nama" value={name} disable={edit} class="mb-2" onChange={handleChange}/>
-                <InputForm id='nim' type='text' name='nim' placeholder="Nomor Induk Mahasiswa" label="Nomor Induk Mahasiswa" value={nim} disable={edit} class="mb-2" onChange={handleChange}/>
-                <InputForm id='prodi' type='text' name='prodi' placeholder="Program Studi" label="Program Studi" value={prodi} disable={edit} class="mb-2" onChange={handleChange}/>
+                <InputForm id='text' type='text' name='username' placeholder="Username" label="Username" value={user.data.username} disable={edit} class="mb-2"/>
                 <InputForm id='email' type='email' name='email' placeholder="Email" label="Email" value={user.data.email} disable={edit} class="mb-2"/>
-                <InputForm id='gol_darah' type='text' name='gol_darah' placeholder="Golongan Darah" label="Golongan Darah" value={gol_darah} disable={edit} class="mb-2" onChange={handleChange}/>
-                <InputForm id='jenis_kelamin' type='text' name='jenis_kelamin' placeholder="Jenis Kelamin" label="Jenis Kelamin" value={jenis_kelamin} disable={edit} class="mb-2" onChange={handleChange}/>
+                <div className="flex mx-auto mb-2">
+                  <Button className="" text="Submit"/>
+                </div>
+                <h1 className="text-center font-bold text-2xl mt-5">Biodata</h1>
+                <div className="flex justify-end">
+                  <Button className="" text="Edit" onClick={() => setEdit(!edit)}/>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <InputForm id='nama' type='text' name='name' placeholder="Nama" label="Nama" value={name} disable={edit} class="mb-2" onChange={handleChange}/>
+                    <InputForm id='nim' type='text' name='nim' placeholder="Nomor Induk Mahasiswa" label="Nomor Induk Mahasiswa" value={nim} disable={edit} class="mb-2" onChange={handleChange}/>
+                    <InputForm id='prodi' type='text' name='prodi' placeholder="Program Studi" label="Program Studi" value={prodi} disable={edit} class="mb-2" onChange={handleChange}/>
+                    <label htmlFor='gol_darah' className="sm:mb-8">
+                        <div className="text-slate-800 mb-2">
+                            Golongan Darah
+                        </div>
+                    <select className="border rounded shadow-lg py-1 px-3 focus:ring-1 focus:border-sky-600 focus:ring-sky-500 focus:outline-none w-full invalid:focus:border-red-500 invalid:focus:ring-red-500 peer" value={gol_darah} name="gol_darah" disabled={edit} onChange={handleChange}>
+                        <option value="default" disabled>Golongan Darah</option>
+                        <option value="A">A</option>
+                        <option value="B">B</option>
+                        <option value="AB">AB</option>
+                        <option value="O">O</option>
+                        <option value="-">-</option>
+                    </select>
+                    </label> 
+                    <label htmlFor='gol_darah' className="sm:mb-8">
+                        <div className="text-slate-800 mb-2">
+                            Jenis Kelamin
+                        </div>
+                    <select className="border rounded shadow-lg py-1 px-3 focus:ring-1 focus:border-sky-600 focus:ring-sky-500 focus:outline-none w-full invalid:focus:border-red-500 invalid:focus:ring-red-500 peer" value={jenis_kelamin} name="jenis_kelamin" disabled={edit} onChange={handleChange}>
+                        <option value="default" disabled>Jenis Kelamin</option>
+                        <option value="Pria">L</option>
+                        <option value="Wanita">P</option>
+                    </select>
+                    </label> 
+                    <div className="flex mx-auto mb-2">
+                      <Button className="" text="Submit"/>
+                    </div>
+                </form>
               </>
             ) : ""}
           </div>
