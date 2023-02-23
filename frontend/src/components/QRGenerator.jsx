d5abc762d1029bd4e91586eef08d3ddc3e7a94d2
maliefrr <aliefm144@gmail.com>
2023-02-23 16:46:01 +0800

    add download ktm function to save html ktm into pdf still need design

--- frontend/src/components/QRGenerator.jsx
+++ frontend/src/components/QRGenerator.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import QRCode from "qrcode.react";
-import {useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 
 const QRGenerator = (props) => {
-    const {user} = useSelector((state) => state.auth)
+    const { user } = useSelector((state) => state.auth);
     const [data, setData] = useState(`http://localhost:3000/profile/${user.data.username}`);
+    const [qrSize, setQRSize] = useState(64);
+    const qrContainerRef = useRef(null);
 
+    useEffect(() => {
+    if (qrContainerRef.current) {
+        const containerWidth = qrContainerRef.current.offsetWidth;
+        const containerHeight = qrContainerRef.current.offsetHeight;
+        const minDimension = Math.min(containerWidth, containerHeight);
+        setQRSize(minDimension);
+    }
+    console.log(qrContainerRef.current)
+    }, []);
+    console.log(qrSize)
     return (
-        <div className={props.className}>
-        <QRCode value={data} size={props.size}/>
-        </div>
+    <div ref={qrContainerRef} className={props.className}>
+        <QRCode value={data} size={qrSize} />
+    </div>
     );
 };
 
