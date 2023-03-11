a7aa5dc6da1650d757d9b1cae24319ef4ef338cd
maliefrr <aliefm144@gmail.com>
2023-03-12 00:00:56 +0800

    change db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Database connected")
     } catch (error) {
         console.error(error);
