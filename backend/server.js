517722616363db1c25bac45478389efc99b96fe9
maliefrr <aliefm144@gmail.com>
2023-03-12 11:20:06 +0800

    fix cors on my netlify app

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use(express.urlencoded({
 }))
 app.use(bodyParser.json())
 app.use(cors({
-    origin: ['https://ektmfrontend-frontend.up.railway.app','http://192.168.196.142:8080','http://localhost:3000'],
+    origin: ['https://ektmfrontend-frontend.up.railway.app','http://192.168.196.142:8080','http://localhost:3000','https://ektm.netlify.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Content-Type', 'Authorization'],
@@ -51,7 +51,7 @@ app.use(cors({
 }));
 // change the trail
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://ektmfrontend-frontend.up.railway.app', 'http://192.168.196.142:8080','http://localhost:3000'); // Replace with your React app's URL
+  res.header('Access-Control-Allow-Origin', 'https://ektmfrontend-frontend.up.railway.app', 'http://192.168.196.142:8080','http://localhost:3000','https://ektm.netlify.app'); // Replace with your React app's URL
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Expose-Headers', 'Content-Type, Authorization');
